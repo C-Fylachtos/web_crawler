284a2bdeff0276f56205cfd65044db6977e288af
C-Fylachtos <talismannnngr@gmail.com>
2022-07-12 17:36:13 +0300

    Final commit before uploading on github

--- crawler.js
+++ crawler.js
@@ -200,9 +200,11 @@ async function getDataFromUrl(newUrl, index, isLastRow) {
   try {
     // const browser = await puppeteer.launch();
     const page = await browser.newPage();
-
+    await page.setUserAgent(
+      'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
+    );
     if (newUrl !== null) {
-      await page.goto(newUrl);
+      await page.goto(newUrl, { waitUntil: 'networkidle0' });
     } else {
       console.log(`Url  value at O${index} is Null`);
       await browser.close();
@@ -518,8 +520,12 @@ async function getUrlFromSku() {
   try {
     // const browser = await puppeteer.launch();
     const page = await browser.newPage();
+    await page.setUserAgent(
+      'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
+    );
     // await page.setViewport({ width: 1366, height: 768 });
     await page.goto(URLS, { waitUntil: 'networkidle0' });
+    // await page.goto(URLS, { waitUntil: 'load' });
 
     const title = await page.title();
     console.log(title);
